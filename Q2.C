d169b85969c173ce184ed016eace3d9e4bf8e61c
Ewerton Santos <ewertonredsnop@gmail.com>
2018-04-22 16:35:17 -0300

    Initial commit

---
+++ Q2.C
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#DEFINE MAX_SIZE 100
+
+struct stack {
+    char element[MAX_SIZE];
+    int top = NULL;
+};
+
+void push(){
+}
+
+void pull(){
+}
+
+void check_top(){
+}
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
